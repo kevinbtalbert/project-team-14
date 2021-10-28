aee5fc2776a2ce0fddfdbc92ff452039568889e9
Kevin Talbert <kevinbtalbert@gmail.com>
2021-10-27 20:35:52 -0400

    Sprint2 complete

--- flaskr/db.py
+++ flaskr/db.py
@@ -1,6 +1,7 @@
 import sqlite3
 import pandas as pd
 from flask import Flask, current_app, g
+from fuzzywuzzy import fuzz
 
 
 def get_db():
@@ -80,6 +81,7 @@ class DB_Commands:
         """
         return query_db("SELECT id, first_name, last_name, email, phone FROM profile", ())
 
+
     @staticmethod
     def find_profile_by_id(id):
         """
@@ -88,6 +90,7 @@ class DB_Commands:
         :return: result of query to database
         """
         return query_db("SELECT id, first_name, last_name, email, phone, dob, gender, address_street, address_city, address_state, address_zip, bust, waist, hip, chest, shoulders, inseam, neck, arm, anniversary FROM profile WHERE id = ?", (id,))
+
 
     @staticmethod
     def find_profile_id_by_name(first_name, last_name):
@@ -98,6 +101,7 @@ class DB_Commands:
         :return: result of query to database
         """
         return query_db("SELECT id FROM profile WHERE first_name = ? AND last_name = ?", (first_name, last_name,))
+
 
     @staticmethod
     def insert_new_profile(first_name="None Provided", last_name="None Provided", email="None Provided", phone="None Provided", dob="None Provided", gender="None Provided", address_street="None Provided", address_city="None Provided", address_state="None Provided", address_zip="None Provided"):
@@ -127,6 +131,7 @@ class DB_Commands:
                 return 0
         else:
             return 0
+
 
     @staticmethod
     def update_profile(id, first_name=None, last_name=None, email=None, phone=None, dob=None, gender=None, address_street=None, address_city=None, address_state=None, address_zip=None, bust=None, waist=None, hip=None, chest=None, shoulders=None, inseam=None, neck=None, arm=None):
@@ -212,6 +217,150 @@ class DB_Commands:
         """
         update_db("DELETE FROM profile WHERE id=?", (id,))
 
+
+    @staticmethod
+    def find_all_products():
+        """
+        Find all products summary information in database
+        """
+        return query_db("SELECT id, product, category, company, department, msrp_cost FROM products", ())
+    
+
+    @staticmethod
+    def find_product_by_id(id):
+        """
+        Method to find a product's information by its id
+        :param id: integer value of id in the database
+        :return: result of query to database
+        """
+        return query_db("SELECT id, product, product_image_url, category, company, company_logo_url, department, msrp_cost, wholesale_cost, manufacturer, inv_quantity, description, last_updated FROM products WHERE id = ?", (id,))
+
+
+    @staticmethod
+    def find_product_id_by_name(product_name):
+        """
+        Method to find a product id by product name
+        Uses fuzzywuzzy module to locate highest likelihood product in database
+        :param product_name: string value of product in the database
+        :return: result of query to database
+        """
+        existing_products = query_db("SELECT product FROM products", ())
+        product_proximity = 0
+        for product in existing_products:
+            for value in product:
+                if fuzz.ratio(value, product_name) > product_proximity:
+                    product_proximity = fuzz.ratio(value, product_name)
+                    product_name = value
+        return query_db("SELECT id FROM products WHERE product = ?", (product_name,))
+
+    
+    @staticmethod
+    def insert_new_product(product="None Provided", product_image_url="None Provided", category="None Provided", company="None Provided", company_logo_url="None Provided", department="None Provided", msrp_cost="None Provided", wholesale_cost="None Provided", manufacturer="None Provided", inv_quantity="None Provided", description="None Provided"):
+        """
+        Command to insert a new profile based on passed in parameters
+        :param product: string value of product in the database (if not supplied, parameter assumes "None Provided")
+        :param product_image_url: string value of product_image_url in the database (if not supplied, parameter assumes "None Provided")
+        :param category: string value of category in the database (if not supplied, parameter assumes "None Provided")
+        :param company: string value of company in the database (if not supplied, parameter assumes "None Provided")
+        :param company_logo_url: string value of company_logo_url in the database (if not supplied, parameter assumes "None Provided")
+        :param department: string value of department in the database (if not supplied, parameter assumes "None Provided")
+        :param msrp_cost: string value of msrp_cost in the database (if not supplied, parameter assumes "None Provided")
+        :param wholesale_cost: string value of wholesale_cost in the database (if not supplied, parameter assumes "None Provided")
+        :param manufacturer: string value of manufacturer in the database (if not supplied, parameter assumes "None Provided")
+        :param inv_quantity: string value of inv_quantity in the database (if not supplied, parameter assumes "None Provided")
+        :param description: string value of inv_quantity in the database (if not supplied, parameter assumes "None Provided")
+        :return: integer value of id, the newest profile in the database
+        """
+        if product != "None Provided":
+            update_db("INSERT INTO products (product, product_image_url, category, company, company_logo_url, department, msrp_cost, wholesale_cost, manufacturer, inv_quantity, description) VALUES (?,?,?,?,?,?,?,?,?,?,?)",
+                      (product, product_image_url, category, company, company_logo_url, department, msrp_cost, wholesale_cost, manufacturer, inv_quantity, description,))
+            try:
+                profile_id = query_db("SELECT max(id) FROM profile", ())
+                for idx in profile_id:
+                    for idy in idx:
+                        return idy
+            except:
+                return 0
+        else:
+            return 0
+
+
+    @staticmethod
+    def update_product(id, product=None, product_image_url=None, category=None, company=None, company_logo_url=None, department=None, msrp_cost=None, wholesale_cost=None, manufacturer=None, inv_quantity=None, description=None):
+        """
+        Command to update particular elements of a customer's profile
+        :param product: string value of product in the database (if not supplied, parameter assumes "None Provided")
+        :param product_image_url: string value of product_image_url in the database (if not supplied, parameter assumes "None Provided")
+        :param category: string value of category in the database (if not supplied, parameter assumes "None Provided")
+        :param company: string value of company in the database (if not supplied, parameter assumes "None Provided")
+        :param company_logo_url: string value of company_logo_url in the database (if not supplied, parameter assumes "None Provided")
+        :param department: string value of department in the database (if not supplied, parameter assumes "None Provided")
+        :param msrp_cost: string value of msrp_cost in the database (if not supplied, parameter assumes "None Provided")
+        :param wholesale_cost: string value of wholesale_cost in the database (if not supplied, parameter assumes "None Provided")
+        :param manufacturer: string value of manufacturer in the database (if not supplied, parameter assumes "None Provided")
+        :param inv_quantity: string value of inv_quantity in the database (if not supplied, parameter assumes "None Provided")
+        :param description: string value of description in the database (if not supplied, parameter assumes "None Provided")
+        """
+        args = locals()
+        for param, value in args.items():
+            if value is not None:
+                if param == "product_image_url":
+                    update_db(
+                        "UPDATE products SET product_image_url=?, last_updated=CURRENT_TIMESTAMP WHERE id=?", (value, id,))
+                if param == "category":
+                    update_db(
+                        "UPDATE products SET category=?, last_updated=CURRENT_TIMESTAMP WHERE id=?", (value, id,))
+                if param == "company":
+                    update_db(
+                        "UPDATE products SET company=?, last_updated=CURRENT_TIMESTAMP WHERE id=?", (value, id,))
+                if param == "company_logo_url":
+                    update_db(
+                        "UPDATE products SET company_logo_url=?, last_updated=CURRENT_TIMESTAMP WHERE id=?", (value, id,))
+                if param == "department":
+                    update_db(
+                        "UPDATE products SET department=?, last_updated=CURRENT_TIMESTAMP WHERE id=?", (value, id,))
+                if param == "msrp_cost":
+                    update_db(
+                        "UPDATE products SET msrp_cost=?, last_updated=CURRENT_TIMESTAMP WHERE id=?", (value, id,))
+                if param == "wholesale_cost":
+                    update_db(
+                        "UPDATE products SET wholesale_cost=?, last_updated=CURRENT_TIMESTAMP WHERE id=?", (value, id,))
+                if param == "manufacturer":
+                    update_db(
+                        "UPDATE products SET manufacturer=?, last_updated=CURRENT_TIMESTAMP WHERE id=?", (value, id,))
+                if param == "inv_quantity":
+                    update_db(
+                        "UPDATE products SET inv_quantity=?, last_updated=CURRENT_TIMESTAMP WHERE id=?", (value, id,))
+                if param == "description":
+                    update_db(
+                        "UPDATE products SET description=?, last_updated=CURRENT_TIMESTAMP WHERE id=?", (value, id,))
+                
+    
+    @staticmethod
+    def delete_product(id):
+        """
+        Command to delete product by id
+        :param id: integer value of id
+        """
+        update_db("DELETE FROM products WHERE id=?", (id,))
+
+
+    @staticmethod
+    def search_module(search_string):
+        """
+        Method to find a product id by product name
+        Uses fuzzywuzzy module to locate highest likelihood product in database
+        :param search_string: string value of what is to be searched in the database
+        :return: result of query to database
+        """
+        existing_products = query_db("SELECT product FROM products", ())
+        product_proximity = 0
+        for product in existing_products:
+            for value in product:
+                if fuzz.ratio(value, product_name) > product_proximity:
+                    product_proximity = fuzz.ratio(value, product_name)
+                    product_name = value
+        return query_db("SELECT id FROM products WHERE product = ?", (product_name,))
 
 class Auth:
 
--- flaskr/products.py
+++ flaskr/products.py
@@ -1,12 +1,165 @@
 from flask import Blueprint, flash, g, redirect, render_template, request, session, url_for
-
-
+from flaskr.db import DB_Commands, sql_to_df
+from flaskr.forms import ProductForm, LookupProductForm, SearchProductById
 bp = Blueprint('products', __name__, url_prefix='/products')
 
 @bp.route('/', methods=('GET', 'POST'))
 def products():
     """
-    Route for the 'product' landing page
+    Route for the 'products' landing page
     :return: rendered template
     """
-    return render_template("products/products.html")
+    headings = ("Product ID", "Product Name", "Category", "Company", "Department", "MSRP", "View Product")
+    df = sql_to_df("SELECT id, product, category, company, department, msrp_cost FROM products")
+    df["Product"] = "View Product"
+    return render_template("products/products.html", headings=headings, data=df)
+
+
+@bp.route('/new', methods=('GET', 'POST'))
+def new_product():
+    """
+    Route for the 'new product' landing page
+    """
+    new_product_form = ProductForm(request.form)
+    if request.method == 'POST':
+        product = new_product_form.data['product']
+        product_image_url = new_product_form.data['product_image_url']
+        category = new_product_form.data['category']
+        company = new_product_form.data['company']
+        company_logo_url = new_product_form.data['company_logo_url']
+        department = new_product_form.data['department']
+        msrp_cost = new_product_form.data['msrp_cost']
+        wholesale_cost = new_product_form.data['wholesale_cost']
+        manufacturer = new_product_form.data['manufacturer']
+        inv_quantity = new_product_form.data['inv_quantity']
+        description = new_product_form.data['description']
+        if new_product_form.data != None:
+            DB_Commands.insert_new_product(product.capitalize(), product_image_url, category, company, company_logo_url, department, msrp_cost, wholesale_cost, manufacturer, inv_quantity, description)
+        return redirect(url_for('products.products'))
+    return render_template("products/new_product.html", form=new_product_form)
+
+
+@bp.route('/lookup', methods=('GET', 'POST'))
+def lookup_product():
+    """
+    Route for the 'lookup product' landing page
+    :return: rendered template
+    """
+    lookup_product_form = LookupProductForm(request.form)
+    
+    if request.method == 'POST':
+        product_name = lookup_product_form.data['product']
+        try:
+            id = DB_Commands.find_product_id_by_name(product_name.capitalize())
+            for value in id:
+                id_to_search = value[0]
+            return redirect(url_for('products.product_by_id', id=id_to_search))
+        except:
+            flash("Profile not found")
+    return render_template("products/lookup_product.html", form=lookup_product_form)
+
+
+@bp.route('/lookup/<id>', methods=('GET', 'POST'))
+def product_by_id(id=None):
+    """
+    Route for looking up a product by id
+    :param id: represents the numerical id of the product in the database (assumes None by default)
+    :return: rendered template
+    """
+    try:
+        search_string = id.data['search']
+    except:
+        search_string = str(id)
+        id = SearchProductById(request.form)
+        id.data['search'] = search_string
+
+    profile_data = DB_Commands.find_product_by_id(search_string)
+    for data in profile_data:
+        profile=data
+
+    return render_template("products/profile_product.html", data=profile)
+
+
+@bp.route('/lookup/<id>/edit_product', methods=('GET', 'POST'))
+def edit_product_by_id(id):
+    """
+    Route for editing product by id
+    :param id: represents the numerical id of the product in the database
+    :return: rendered template
+    """
+    edit_product_form = ProductForm(request.form)
+    try:
+        search_string = id.data['search']
+    except:
+        search_string = str(id)
+        id = SearchProductById(request.form)
+        id.data['search'] = search_string
+    
+    if request.method == "POST":
+        product = edit_product_form.data['product']
+        if product != "":
+            DB_Commands.update_product(search_string, product=product)
+        product_image_url = edit_product_form.data['product_image_url']
+        if product_image_url != "":
+            DB_Commands.update_product(search_string, product_image_url=product_image_url)
+        category = edit_product_form.data['category']
+        if category != "":
+            DB_Commands.update_product(search_string, category=category)
+        company = edit_product_form.data['company']
+        if company != "":
+            DB_Commands.update_product(search_string, company=company)
+        company_logo_url = edit_product_form.data['company_logo_url']
+        if company_logo_url != "":
+            DB_Commands.update_product(search_string, company_logo_url=company_logo_url)
+        department = edit_product_form.data['department']
+        if department != "":
+            DB_Commands.update_product(search_string, department=department)
+        msrp_cost = edit_product_form.data['msrp_cost']
+        if msrp_cost != "":
+            DB_Commands.update_product(search_string, msrp_cost=msrp_cost)
+        wholesale_cost = edit_product_form.data['wholesale_cost']
+        if wholesale_cost != "":
+            DB_Commands.update_product(search_string, wholesale_cost=wholesale_cost)
+        manufacturer = edit_product_form.data['manufacturer']
+        if manufacturer != "":
+            DB_Commands.update_product(search_string, manufacturer=manufacturer)
+        inv_quantity = edit_product_form.data['inv_quantity']
+        if inv_quantity != "":
+            DB_Commands.update_product(search_string, inv_quantity=inv_quantity)
+        description = edit_product_form.data['description']
+        if description != "":
+            DB_Commands.update_product(search_string, description=description)
+
+        return redirect(url_for('products.product_by_id', id=search_string))
+
+    product_data = DB_Commands.find_product_by_id(search_string)
+    for data in product_data:
+        product=data
+
+    return render_template("products/edit_product.html", data=product, form=edit_product_form)
+
+
+@bp.route('/lookup/<id>/delete_product', methods=('GET', 'POST'))
+def delete_product_by_id(id):
+    """
+    Route for deleting product by id
+    :param id: represents the numerical id of the product in the database
+    :return: rendered template
+    """
+    edit_product_form = ProductForm(request.form)
+    try:
+        search_string = id.data['search']
+    except:
+        search_string = str(id)
+        id = SearchProductById(request.form)
+        id.data['search'] = search_string
+    
+    product_data = DB_Commands.find_product_by_id(search_string)
+    for data in product_data:
+        product=data
+
+    if request.method == "POST":
+        DB_Commands.delete_product(search_string)
+        return redirect(url_for('products.products'))
+
+    return render_template("products/delete_product.html", data=product, form=edit_product_form)
